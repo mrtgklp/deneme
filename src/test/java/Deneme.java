1d22f33fd2234611f25d2ddb64b831613b7ad2f8
mrtgklp <109852097+mrtgklp@users.noreply.github.com>
2023-02-26 13:57:55 +0300

    v3

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -2,5 +2,6 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Dünya");
+        System.out.println("B129");
     }
 }
